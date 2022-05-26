2ae599c1b68287da49e7127ace11e23a0554d04d
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2022-05-26 15:57:33 -0300

    Add files via upload

---
+++ Funções.py
@@ -0,0 +1,37 @@
+'''
+def func(msg, msg2):
+    print(msg, msg2)
+
+func("Olá", True)
+
+def procedure(msg):
+    print(msg)
+
+procedure("Hello World!")
+
+def area(largura,comprimento):
+    resultado = print(largura * comprimento)
+    return resultado
+
+area(10, 20)
+
+def maior_menor(num1, num2):
+    if num1 < num2:
+        print("{} Maior que o {}".format(num2,num1))
+    elif num1 > num2:
+        print("{} Menor que o {}".format(num2, num1))
+    else:
+        print("Eles são iguais")
+    return maior_menor
+
+maior_menor(5, 6)
+'''
+def comp(a, b, c):
+    soma = a + b
+    if soma < c:
+        return True
+    else:
+        return False
+
+
+res = comp(1, 2, 3)
