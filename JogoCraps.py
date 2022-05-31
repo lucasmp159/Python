3d71baeb0254f837b6f9062d7f8ec366a99acbba
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2022-05-31 12:44:16 -0300

    Update JogoCraps.py

--- JogoCraps.py
+++ JogoCraps.py
@@ -2,33 +2,24 @@ import random
 
 def jogo_de_crap():
     while True:
-        count = 0
         selecionado = 0
         dados = random.randint(2,12)
+        print (dados)
         if dados in [7, 12]:
-            selecionado = dados
-            print("natural " + str(selecionado))
+            print("natural " + str(dados))
             print("Ganhou")
             break
         if dados in [2, 3, 11]:
-            count = 0
-            selecionado = dados
-            print("craps " + str(selecionado))
+            print("craps " + str(dados))
             print("Perdeu")
             break
         if dados in [4, 5, 6, 8, 9, 10]:
-            count += 1
-            selecionado = dados
             print("Ponto " + str(selecionado))
-            if count > 0 and dados == 7:
-                print("perdeu")
-                break
-            elif count > 0 and dados in [2, 3, 12]:
-                print("perdeu")
-                break
-            elif dados != selecionado: 
-                print("Jogue novamente")
-            else:
+            if selecionado == dados:
                 print("Ganhou repetindo o ponto")
                 break
+            else:
+                selecionado = dados
+                print("Jogue novamente")
+               
 jogo_de_crap()
